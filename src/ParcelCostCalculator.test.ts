e443cd870ce6d81ba32b845b3bac30f4391e9f96
Oscar Sims <oscar@oscarsims.co.nz>
2020-10-12 20:46:02 +1300

    Add conditions for all parcel sizes and tests for each type of parcel.

--- src/ParcelCostCalculator.test.ts
+++ src/ParcelCostCalculator.test.ts
@@ -8,4 +8,31 @@ test('A single small parcel', () => {
     expect(result.costs.get(parcels[0])).toBe(3);
     expect(result.types.get(parcels[0])).toBe(ParcelType.Small);
     expect(result.total).toBe(3);
-})
+});
+
+test('A single medium parcel', () => {
+    let parcels = [new Parcel(9.6, 15, 48)]
+    let result = ParcelCostCalculator.calculate(parcels);
+
+    expect(result.costs.get(parcels[0])).toBe(8);
+    expect(result.types.get(parcels[0])).toBe(ParcelType.Medium);
+    expect(result.total).toBe(8);
+});
+
+test('A single large parcel', () => {
+    let parcels = [new Parcel(50, 99.8, 10)]
+    let result = ParcelCostCalculator.calculate(parcels);
+
+    expect(result.costs.get(parcels[0])).toBe(15);
+    expect(result.types.get(parcels[0])).toBe(ParcelType.Large);
+    expect(result.total).toBe(15);
+});
+
+test('A single extra large parcel', () => {
+    let parcels = [new Parcel(50, 99.8, 102)]
+    let result = ParcelCostCalculator.calculate(parcels);
+
+    expect(result.costs.get(parcels[0])).toBe(25);
+    expect(result.types.get(parcels[0])).toBe(ParcelType.ExtraLarge);
+    expect(result.total).toBe(25);
+});
--- src/ParcelCostCalculator.ts
+++ src/ParcelCostCalculator.ts
@@ -23,6 +23,18 @@ export class ParcelCostCalculator {
                 costs.set(parcel, 3);
                 types.set(parcel, ParcelType.Small);
             }
+            else if (parcel.width < 50 && parcel.height < 50 && parcel.depth < 50) {
+                costs.set(parcel, 8);
+                types.set(parcel, ParcelType.Medium);
+            }
+            else if (parcel.width < 100 && parcel.height < 100 && parcel.depth < 100) {
+                costs.set(parcel, 15);
+                types.set(parcel, ParcelType.Large);
+            }
+            else {
+                costs.set(parcel, 25);
+                types.set(parcel, ParcelType.ExtraLarge);
+            }
         }
 
         let total = Array.from(costs.values())
